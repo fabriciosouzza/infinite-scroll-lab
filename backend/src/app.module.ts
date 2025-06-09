c90107e00a29d158b4ce4153f2b869e4d778ab06
Fabrício Souza <fabriciossza@gmail.com>
2025-06-08 21:10:37 -0300

    feat: add product backend boilerplate

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductModule } from './product/product.module';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
+import { PostgreSqlDriver } from '@mikro-orm/postgresql';
 
 @Module({
-  imports: [],
+  imports: [
+    ProductModule,
+    MikroOrmModule.forRoot({
+      entities: ['./dist/entities'],
+      entitiesTs: ['./src/entities'],
+      dbName: 'products',
+      driver: PostgreSqlDriver,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
