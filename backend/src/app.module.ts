4ae0ed07b90b05b5577d284b378271f9bc5414e5
Fabrício Souza <fabriciossza@gmail.com>
2025-06-10 06:07:35 -0300

    feat: add simple ttl revalidation cache

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,15 +4,20 @@ import { MikroOrmModule } from '@mikro-orm/nestjs';
 import config from 'mikro-orm.config';
 import { OrderModule } from './order/order.module';
 import { CoreModule } from './core/core.module';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { CacheInterceptor, CacheModule } from '@nestjs/cache-manager';
 
 @Module({
   imports: [
+    CacheModule.register({
+      ttl: 5000,
+    }),
     MikroOrmModule.forRoot(config),
     ProductModule,
     OrderModule,
     CoreModule,
   ],
   controllers: [],
-  providers: [],
+  providers: [{ provide: APP_INTERCEPTOR, useClass: CacheInterceptor }],
 })
 export class AppModule {}
