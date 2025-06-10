f33ddc8df614718ced6e6e18ec37cd2ad40e8c92
fabriciosouzza <fabriciossza@gmail.com>
2025-06-10 17:08:52 -0300

    style: remove unused imports

--- frontend/components/NestedList.tsx
+++ frontend/components/NestedList.tsx
@@ -1,18 +1,10 @@
-import ListSubheader from "@mui/material/ListSubheader";
 import List from "@mui/material/List";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
 import Collapse from "@mui/material/Collapse";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import DraftsIcon from "@mui/icons-material/Drafts";
-import SendIcon from "@mui/icons-material/Send";
 import ExpandLess from "@mui/icons-material/ExpandLess";
 import ExpandMore from "@mui/icons-material/ExpandMore";
-import StarBorder from "@mui/icons-material/StarBorder";
 import { useState } from "react";
 import { Order } from "@/model/order";
-import { Box, Divider, ListItem, Stack, Typography } from "@mui/material";
+import { Box, Divider, Typography } from "@mui/material";
 import getEquivalentReviewStatus from "@/utils/getEquivalentReviewStatus";
 
 interface NestedListProps {
