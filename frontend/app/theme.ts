d32bf8d41cb3b4a43f3f632b342d536f1f38a0a4
Fabrício Souza <fabriciossza@gmail.com>
2025-06-10 06:42:32 -0300

    feat: add mui and clean nextjs boilerplate

---
+++ frontend/app/theme.ts
@@ -0,0 +1,13 @@
+"use client";
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  colorSchemes: {},
+  cssVariables: {
+    colorSchemeSelector: "class",
+  },
+  typography: {},
+  components: {},
+});
+
+export default theme;
